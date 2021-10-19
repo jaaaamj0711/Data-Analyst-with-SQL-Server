23a4f8211f728e8e0c8a5276a1f65ac9fa5d276f
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-10-19 13:28:43 +0900

    Update Groups_strings_and counting things.sql

--- Introduction to SQL Server/Groups_strings_and counting things.sql
+++ Introduction to SQL Server/Groups_strings_and counting things.sql
@@ -90,3 +90,93 @@ SELECT TOP (10)
 FROM 
   grid
 WHERE description LIKE '%Weather%';
+
+-- Select the region column
+SELECT 
+  nerc_region,
+  -- Sum the demand_loss_mw column
+  SUM(demand_loss_mw) AS demand_loss
+FROM 
+  grid
+  -- Exclude NULL values of demand_loss
+WHERE 
+  demand_loss_mw IS NOT NULL
+  -- Group the results by nerc_region
+GROUP BY
+  nerc_region
+  -- Order the results in descending order of demand_loss
+ORDER BY 
+  demand_loss DESC;
+  
+SELECT 
+  nerc_region, 
+  SUM (demand_loss_mw) AS demand_loss 
+FROM 
+  grid 
+  -- Remove the WHERE clause
+GROUP BY 
+  nerc_region 
+  -- Enter a new HAVING clause so that the sum of demand_loss_mw is greater than 10000
+HAVING 
+  SUM(demand_loss_mw) > 10000 
+ORDER BY 
+  demand_loss DESC;
+  
+-- Retrieve the minimum and maximum place values
+SELECT 
+  -- the lower the value the higher the place, so MIN = the highest placing
+  MIN(place) AS hi_place, 
+  MAX(place) AS lo_place, 
+  -- Retrieve the minimum and maximum points values
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision;
+  
+-- Retrieve the minimum and maximum place values
+SELECT 
+  -- the lower the value the higher the place, so MIN = the highest placing
+  MIN(place) AS hi_place, 
+  MAX(place) AS lo_place, 
+  -- Retrieve the minimum and maximum points values
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision
+  -- Group by country
+GROUP BY
+  country;
+  
+-- Obtain a count for each country
+SELECT 
+  COUNT(country) AS country_count, 
+  -- Retrieve the country column
+  country, 
+  -- Return the average of the Place column 
+  AVG(place) AS average_place, 
+  AVG(points) AS avg_points, 
+  MIN(points) AS min_points, 
+  MAX(points) AS max_points 
+FROM 
+  eurovision 
+GROUP BY 
+  country;
+  
+SELECT 
+  country, 
+  COUNT (country) AS country_count, 
+  AVG (place) AS avg_place, 
+  AVG (points) AS avg_points, 
+  MIN (points) AS min_points, 
+  MAX (points) AS max_points 
+FROM 
+  eurovision 
+GROUP BY 
+  country 
+  -- The country column should only contain those with a count greater than 5
+HAVING
+  COUNT(country) > 5 
+  -- Arrange columns in the correct order
+ORDER BY 
+  avg_place, 
+  avg_points DESC;
